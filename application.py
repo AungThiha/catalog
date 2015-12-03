2600a51ed47161b01996e8b62913e996b82f9f52
AungThiha <mr.aungthiha@gmail.com>
2015-12-04 05:34:26 +0630

    edit routes params

--- application.py
+++ application.py
@@ -8,32 +8,32 @@ def show_home():
 
 
 @app.route('/catalog/<int:catalog_id>/')
-def show_catalog():
+def show_catalog(catalog_id):
     return 'show catalog'
 
 
 @app.route('/catalog/<int:catalog_id>.json')
-def show_catalog_json():
+def show_catalog_json(catalog_id):
     return 'show catalog'
 
 
 @app.route('/catalog/<int:catalog_id>/new')
-def add_item():
+def add_item(catalog_id):
     return 'add item'
 
 
 @app.route('/catalog/<int:catalog_id>/<int:item_id>/')
-def show_item():
+def show_item(catalog_id, item_id):
     return 'show item'
 
 
 @app.route('/catalog/<int:catalog_id>/<int:item_id>/edit')
-def edit_item():
+def edit_item(catalog_id, item_id):
     return 'edit item'
 
 
 @app.route('/catalog/<int:catalog_id>/<int:item_id>/delete')
-def edit_item():
+def edit_item(catalog_id, item_id):
     return 'delete item'
 
 
